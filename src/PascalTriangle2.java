6671dd2c94a1658a35a9cc72e9edc9b582928a63
Tai-An Chen <soniccol@gmail.com>
2015-01-13 14:57:41 +0800

    encontered java integer overflow problem in pascal 30th level, fixed.

---
+++ src/PascalTriangle2.java
@@ -0,0 +1,61 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalTriangle {
+
+	public static void main(String[] args) {
+		PascalTriangle PascalTriangle = new PascalTriangle();
+		System.out.println(PascalTriangle.getRow(30));
+	}
+	
+	public List<List<Integer>> generate(int numRows) {
+		List<List<Integer>> triangle=new ArrayList<List<Integer>>();
+		for(int i=1;i<=numRows;i++){
+			triangle.add(getRow(i));
+		}
+		return triangle;
+    }
+	public List<Integer> getRow(int number){
+		List<Integer> onerow=new ArrayList<Integer>();
+		List<Integer> reverseOne=new ArrayList<Integer>();
+		if(number==0){
+			onerow.add(1);
+			return onerow;			
+		}else if(number==1){
+			onerow.add(1);
+			onerow.add(1);
+			return onerow;
+		}else{
+			number++;			
+			if(number%2==0){			
+				for(int i=1;i<=(number/2);i++){
+					int comvalue=combi(number-1, i-1);
+					onerow.add(comvalue);
+					reverseOne.add(comvalue);
+				}
+				for(int i=reverseOne.size()-1;i>=0;i--){
+					onerow.add(reverseOne.get(i));
+				}				
+			}else{
+				for(int i=1;i<=(number/2)+1;i++){
+					int comvalue=combi(number-1, i-1);
+					onerow.add(comvalue);
+					reverseOne.add(comvalue);
+				}
+				for(int i=reverseOne.size()-2;i>=0;i--){
+					onerow.add(reverseOne.get(i));
+				}
+			}
+			return onerow;
+		}
+	}
+	public int combi(int r, int n){
+		double p=1;
+		for(int i = 1; i <= n; i++){
+	       	p = p * (r - i + 1) / i;
+		}
+	    return (int)p;
+	}
+}
