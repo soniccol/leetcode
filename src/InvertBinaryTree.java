08e476bc3ae840ac129a9a2b2855ce7b4c492dbd
Tai-An Chen <soniccol@gmail.com>
2015-07-24 17:30:20 +0800

    Create InvertBinaryTree.java
    I solved this problem, I can work in Google now ? In this problem only provide only solution code without main function.

---
+++ src/InvertBinaryTree.java
@@ -0,0 +1,26 @@
+/**
+ *  * Definition for a binary tree node.
+ *   * public class TreeNode {
+ *    *     int val;
+ *     *     TreeNode left;
+ *      *     TreeNode right;
+ *       *     TreeNode(int x) { val = x; }
+ *        * }
+ *         */
+public class InvertBinaryTree {
+public TreeNode invertTree(TreeNode root) {
+     if(root==null) return root;
+     if(root.right==null&&root.left==null) return root;
+
+      TreeNode temp=null;
+      temp=root.right;
+      root.right=root.left;
+      root.left=temp;
+
+      if(root.right!=null) invertTree(root.right);
+      if(root.left!=null) invertTree(root.left);
+
+      return root;
+
+}
+}
