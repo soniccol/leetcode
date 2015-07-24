c57d2e9e60e0a6cf630e2d38dfd155fc2c37695f
Tai-An Chen <soniccol@gmail.com>
2015-07-24 17:52:13 +0800

    Create LinkedListCycle.java
    My manager used to test interviewer as an advanced coding problem, I haven't solved this problem before, now I know how to use two pointers to detect singly-linked list cycle.

---
+++ src/LinkedListCycle.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+public boolean hasCycle(ListNode head) {
+     ListNode slow=head;
+     ListNode fast=head;
+     while(slow!=null&&fast!=null&&fast.next!=null){
+          slow=slow.next;
+          if(fast.next.next==null) return false;
+          else fast=fast.next.next;
+          
+          if(fast.val==slow.val){
+               return true;
+          }
+     }
+     return false;
+}
+}
