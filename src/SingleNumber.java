91c24614c7973bb8fcfbb42881cb1f444dc2cce8
Tai-An Chen <soniccol@gmail.com>
2015-08-06 16:00:07 +0800

    Create SingleNumber.java
    Not a good way to detect a singleNumber in an array, cost much resources.

---
+++ src/SingleNumber.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int singleNumber(int[] nums) {
+    	java.util.Map<Integer, Integer> map = new java.util.HashMap<Integer, Integer>();
+    	for(int i=0;i<nums.length;i++){
+    		Integer data=map.get(nums[i]);
+    		if(data==null) map.put(nums[i],1);
+    		else{
+    			data++;
+    			map.put(nums[i], data);
+    		}
+    	}
+    	for(java.util.Map.Entry<Integer, Integer> entry:map.entrySet()){
+    		entry.getKey();
+    		if(entry.getValue()==1) return entry.getKey().intValue();
+    	}
+    	
+    	if(nums.length==1) return nums[0];
+        	
+    	return 0;
+    }
+}
