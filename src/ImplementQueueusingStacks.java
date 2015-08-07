7c02ef7785bc939de6a7675e3734bbb67c8b0bc5
Tai-An Chen <soniccol@gmail.com>
2015-08-07 15:51:47 +0800

    Create ImplementQueueusingStacks.java
    Stack is a first in last out data structure, we need to use two stacks to put the data like in the queue.

---
+++ src/ImplementQueueusingStacks.java
@@ -0,0 +1,42 @@
+class MyQueue {
+    
+    
+	java.util.Stack<Integer> inbox=new java.util.Stack<Integer>();
+	java.util.Stack<Integer> outbox=new java.util.Stack<Integer>();
+	
+    // Push element x to the back of queue.
+    public void push(int x) {
+    	inbox.push(x);
+    }
+
+    // Removes the element from in front of queue.
+    public void pop() {
+        if(outbox.isEmpty()){
+        	while(!inbox.isEmpty()){
+        		outbox.push(inbox.pop());
+        	}
+        }
+        outbox.pop();
+    }
+
+    // Get the front element.
+    public int peek() {
+    	if(outbox.isEmpty()){
+        	while(!inbox.isEmpty()){
+        		outbox.push(inbox.pop());
+        	}
+        }
+        return outbox.peek();
+    }
+
+    // Return whether the queue is empty.
+    public boolean empty() {
+        if(outbox.isEmpty()){
+        	while(!inbox.isEmpty()){
+        		outbox.push(inbox.pop());
+        	}
+        }
+        return outbox.isEmpty();
+    }
+
+}
