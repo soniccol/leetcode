9ddb6d5c499f4fa22bdf110a11aa3b79743f1c08
Tai-An Chen <soniccol@gmail.com>
2015-08-07 14:21:59 +0800

    Create MajorityElement.java
    Using the solving pattern by SingleNumber.java.

---
+++ src/MajorityElement.java
@@ -0,0 +1,26 @@
+public class MajorityElement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+    public int majorityElement(int[] nums) {
+        
+    	int times=(int)Math.ceil(nums.length/2);
+    	
+    	java.util.Map<Integer, Integer> map = new java.util.HashMap<Integer, Integer>();
+    	for(int i=0;i<nums.length;i++){
+    		Integer data=map.get(nums[i]);
+    		if(data==null) map.put(nums[i],1);
+    		else{
+    			data++;
+    			map.put(nums[i], data);
+    		}
+    	}
+    	for(java.util.Map.Entry<Integer, Integer> entry:map.entrySet()){    		
+    		if(entry.getValue()>times) return entry.getKey().intValue();
+    	}
+    	
+    	return 0;
+    }
+}
