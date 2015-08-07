21bcda2e73508d1c16ce4b2e0ee3fc757f7d4f7a
Tai-An Chen <soniccol@gmail.com>
2015-08-07 10:12:15 +0800

    Create Numberof1Bits.java
    There is no unsigned int in Java, I converted integer to binary string to detect whether it contains how much 1 in the string.

---
+++ src/Numberof1Bits.java
@@ -0,0 +1,16 @@
+public class Numberof1Bits {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(hammingWeight(2));
+	}
+    // you need to treat n as an unsigned value
+    public static int hammingWeight(int n) {
+        String s=Integer.toBinaryString(n);
+    	int result=0;
+        for(int i=0;i<s.length();i++){
+        	if(s.substring(i, i+1).equals("1")) result++;        	
+        }
+    	return result;
+    }
+}
