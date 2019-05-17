10e975bf33121a55a8fa75f7b79ecf9f1f78e511
Tai-An Chen <soniccol@gmail.com>
2019-05-17 14:17:58 +0800

    Add reverse string using two pointers.

---
+++ src/reverseString.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int front=0;
+        int rear=s.length-1;
+               
+        while(front<rear){
+            char c = s[front];
+            s[front]=s[rear];
+            s[rear]=c;
+            front++;
+            rear--;
+        }        
+    }
+}
