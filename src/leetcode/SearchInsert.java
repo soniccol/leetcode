def3e84c0ac2d69efd30a7d08a7a2196b15bb562
Tai-An Chen <soniccol@gmail.com>
2019-09-27 13:34:38 +0800

    Add SearchInsert.java

---
+++ src/leetcode/SearchInsert.java
@@ -0,0 +1,15 @@
+class SearchInsert {
+    public int searchInsert(int[] nums, int target) {
+        
+        if(nums.length==0 || target==0) return 0;
+        
+        
+        for(int i=0;i<nums.length;i++){
+            if(target<=nums[i]){
+                return i;
+            }
+        }
+                
+        return nums.length;
+    }
+}
