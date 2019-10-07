18796fa78708d209bdc10848464397130655009c
Tai-An Chen <soniccol@gmail.com>
2019-10-07 15:42:31 +0800

    Update ContainsDuplicate.java

--- src/leetcode/ContainsDuplicate.java
+++ src/leetcode/ContainsDuplicate.java
@@ -13,4 +13,25 @@ public class ContainsDuplicate {
     	}
         return false;
     }
+    
+    //Time Complexity O(nlogn), Space Complexity O(1)
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);    
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==nums[i-1]) return true;
+        }
+        return false;
+    }
+    
+    //Time Complexity O(n), Space Complexity O(n)
+    public boolean containsDuplicate(int[] nums) {
+        
+        Set set = new HashSet();
+        for(int i=0;i<nums.length;i++){
+            if(!set.add(nums[i])) return true;
+        }
+        return false;
+    }
+
+    
 }
